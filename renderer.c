814a9986901c93fe847f56864269228bbc6b56ac
yul <aymeric.guth@protonmail.com>
2022-09-11 22:08:01 +0200

    added color

--- renderer.c
+++ renderer.c
@@ -1,5 +1,7 @@
 #include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include "utilz.h"
 
@@ -7,6 +9,28 @@
 #define SCREEN_WIDTH 124 / 2
 #define SCREEN_HEIGHT 33
 #define DEBUG 0
+#define CYCLES 100
+
+
+typedef enum { NONE, BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE } Color;
+
+typedef struct {
+    int shader;
+    Color color;
+} Pixel;
+
+const char *color_map[] = {
+    [NONE] = "",
+    [BLACK] = "\x1b[30;1m",
+    [RED] = "\x1b[31;1m",
+    [GREEN] = "\x1b[32;1m",
+    [YELLOW] = "\x1b[33;1m",
+    [BLUE] = "\x1b[34;1m",
+    [MAGENTA] = "\x1b[35;1m",
+    [CYAN] = "\x1b[36;1m",
+    [WHITE] = "\x1b[37;1m",
+};
+
 
 void moveline(int space3d[SIZE3D][SIZE3D][SIZE3D], int n)
 {
@@ -24,36 +48,27 @@ void moveline(int space3d[SIZE3D][SIZE3D][SIZE3D], int n)
     }
 }
 
-void _render(int space2d[SCREEN_HEIGHT][SCREEN_WIDTH])
+void render(Pixel space2d[SCREEN_HEIGHT][SCREEN_WIDTH])
 {
     printf("\x1b[H");
 
     for (int j = 0; j < SCREEN_HEIGHT; j++) {
         for (int i = 0; i < SCREEN_WIDTH; i++) {
-            int L = space2d[j][i];
-            printf("%c", " .,-~:;=!*#$@"[L]);
+            Pixel px = space2d[j][i];
+            char c = " .,-~:;=!*#$@"[px.shader];
+
+            if (px.color) {
+                const char *color = color_map[px.color];
+                printf("%s", color);
+            }
+
+            printf("%c%c\x1b[0m", c, c);
         }
 
         printf("\n");
     }
 }
 
-void render(int space2d[SCREEN_HEIGHT][SCREEN_WIDTH])
-{
-    printf("\x1b[H");
-
-    for (int j = 0; j < SCREEN_HEIGHT; j++) {
-        for (int i = 0; i < SCREEN_WIDTH; i++) {
-            int L = space2d[j][i];
-            printf("\x1b[31;1m");
-            printf("%c", " .,-~:;=!*#$@"[L]);
-            printf("%c", " .,-~:;=!*#$@"[L]);
-            printf("\x1b[0m");
-        }
-
-        printf("\n");
-    }
-}
 
 float linear_interpolation(
     float src_min,
@@ -66,8 +81,11 @@ float linear_interpolation(
     return (x - src_min) * (dst_max - dst_min) / (src_max - src_min) + dst_min;
 }
 
-
-void transform(int space3d[SIZE3D][SIZE3D][SIZE3D], int space2d[SCREEN_HEIGHT][SCREEN_WIDTH])
+void transform(
+    Pixel space3d[SIZE3D][SIZE3D][SIZE3D],
+    Pixel space2d[SCREEN_HEIGHT][SCREEN_WIDTH],
+    float zoom
+)
 {
     float zbuffer[SCREEN_HEIGHT][SCREEN_WIDTH];
 
@@ -79,7 +97,7 @@ void transform(int space3d[SIZE3D][SIZE3D][SIZE3D], int space2d[SCREEN_HEIGHT][S
     // camera distance from the screen
     float K1 = 1.f;
     // screen distance from the scene
-    float K2 = 10.f;
+    float K2 = 1.f * zoom;
 
     for (int k = 0; k < SIZE3D; k++) {
         for (int j = 0; j < SIZE3D; j++) {
@@ -103,38 +121,74 @@ void transform(int space3d[SIZE3D][SIZE3D][SIZE3D], int space2d[SCREEN_HEIGHT][S
 
                 if (ooz > zbuffer[yp][xp]) {
                     zbuffer[yp][xp] = ooz;
-                    space2d[yp][xp] = space3d[k][j][i];
+                    memcpy(&space2d[yp][xp], &space3d[k][j][i], sizeof(Pixel));
+                    //space2d[yp][xp] = space3d[k][j][i];
                 }
             }
         }
     }
 }
 
-int main()
+void colorize(Pixel space3d[SIZE3D][SIZE3D][SIZE3D])
 {
-    int space3d[SIZE3D][SIZE3D][SIZE3D];
-    int space2d[SCREEN_HEIGHT][SCREEN_WIDTH];
+    for (int j = 0; j < SIZE3D; j++) {
+        for (int i = 0; i < SIZE3D; i++) {
+            Pixel *px = &(space3d[0][j][i]);
+            px->color = RED;
+            px = &space3d[SIZE3D - 1][j][i];
+            px->color = CYAN;
+        }
+    }
 
     for (int k = 0; k < SIZE3D; k++) {
         for (int j = 0; j < SIZE3D; j++) {
-            for (int i = 0; i < SIZE3D; i++)
-                space3d[k][j][i] = 1;
+            Pixel *px = &space3d[k][j][1];
+            px->color = GREEN;
+            px = &space3d[k][j][SIZE3D - 1];
+            px->color = BLUE;
+        }
+    }
+
+    for (int k = 0; k < SIZE3D; k++) {
+        for (int i = 0; i < SIZE3D; i++) {
+            Pixel *px = &space3d[k][0][i];
+            px->color = WHITE;
+            px = &space3d[k][SIZE3D - 1][i];
+            px->color = YELLOW;
+        }
+    }
+}
+
+int main()
+{
+    Pixel space3d[SIZE3D][SIZE3D][SIZE3D];
+    Pixel space2d[SCREEN_HEIGHT][SCREEN_WIDTH];
+
+    for (int k = 0; k < SIZE3D; k++) {
+        for (int j = 0; j < SIZE3D; j++) {
+            for (int i = 0; i < SIZE3D; i++) {
+                Pixel px = {.color = NONE, .shader = 1};
+                space3d[k][j][i] = px;
+            }
         }
     }
 
     for (int j = 0; j < SCREEN_HEIGHT; j++) {
-        for (int i = 0; i < SCREEN_WIDTH; i++)
-            space2d[j][i] = 0;
+        for (int i = 0; i < SCREEN_WIDTH; i++) {
+            Pixel px = {.color = NONE, .shader = 0};
+            space2d[j][i] = px;
+        }
     }
 
+    colorize(space3d);
     // transform(space3d, space2d);
     // return 0;
 
-    for (int n = 0; n < SIZE3D; n++) {
+    for (int n = 0; n < CYCLES; n++) {
         // moveline(space3d, n);
-        transform(space3d, space2d);
+        transform(space3d, space2d, 3.f);
         render(space2d);
-        msleep(100);
+        msleep(17);
     }
 
     return 0;
