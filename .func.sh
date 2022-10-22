ff626b053594015da49acae7e3566019a284f577
yul <aymeric.guth@protonmail.com>
2022-10-23 01:55:53 +0200

    env

--- .func.sh
+++ .func.sh
@@ -14,12 +14,12 @@ build() {
 
 run() {
   [ ! "$(basename "$PWD")" = "$PROJECT_NAME" ] && return 1
-  OBJ_FILE="cube.obj" python3 obj_loader.py || return 1
-  # OBJ_FILE="teapot2.obj" python3 obj_loader.py || return 1
+  # OBJ_FILE="cube.obj" python3 obj_loader.py || return 1
+  OBJ_FILE="teapot2.obj" python3 obj_loader.py || return 1
   build || return 1
   # GPU accelerated Panel, only for X11
   # kitty --config=./kitty.conf --start-as=maximized +kitten panel sh -c "./build/$PROJECT_NAME"
   # kitty --config=./kitty.conf --start-as=maximized sh -c "./a.out"
-  kitty --config=./kitty.conf --start-as=maximized sh -c "./build/$PROJECT_NAME"
+  kitty --config=./kitty.conf --start-as=maximized sh -c "./build/$PROJECT_NAME" || return 1
   # ./build/"$PROJECT_NAME" && echo "$?"
 }
