f75b1ad3fbbe3abdb5c649c579d985000552e1e5
yul <aymeric.guth@protonmail.com>
2022-11-18 20:45:20 +0100

    feature: character rendering via alacritty

--- .func.sh
+++ .func.sh
@@ -3,27 +3,28 @@
 [ -f "$DOTFILES/funcs/project_c.sh" ] && . "$DOTFILES/funcs/project_c.sh"
 
 clean() {
-  # c_project_clean || return 1
-  c_clean || return 1
+	# c_project_clean || return 1
+	c_clean_cmake || return 1
 }
 
 build() {
-  c_clean || return 1
-  c_build_cmake || return 1
+	c_clean_cmake || return 1
+	c_build_cmake || return 1
 }
 
 gen() {
-  c_tools_gen || return 1
+	c_tools_gen || return 1
 }
 
 run() {
-  c_project_run || return 1
-  # OBJ_FILE="cube.obj" python3 obj_loader.py || return 1
-  OBJ_FILE="teapot2.obj" python3 obj_loader.py || return 1
-  build || return 1
-  # GPU accelerated Panel, only for X11
-  # kitty --config=./kitty.conf --start-as=maximized +kitten panel sh -c "./build/$PROJECT_NAME"
-  # kitty --config=./kitty.conf --start-as=maximized sh -c "./a.out"
-  kitty --config=./kitty.conf --start-as=maximized sh -c "./build/$PROJECT_NAME" || return 1
-  # ./build/"$PROJECT_NAME" && echo "$?"
+	c_project_run || return 1
+	# OBJ_FILE="cube.obj" python3 obj_loader.py || return 1
+	OBJ_FILE="teapot2.obj" python3 obj_loader.py || return 1
+	build || return 1
+	# GPU accelerated Panel, only for X11
+	# kitty --config=./kitty.conf --start-as=maximized +kitten panel sh -c "./build/$PROJECT_NAME"
+	# kitty --config=./kitty.conf --start-as=maximized sh -c "./a.out"
+	alacritty --config-file ./alacritty.yml --command "$WORKSPACE/build/$PROJECT_NAME" || return 1
+	# kitty --config=./kitty.conf --start-as=maximized sh -c "./build/$PROJECT_NAME" || return 1
+	# ./build/"$PROJECT_NAME" && echo "$?"
 }
